14a312ac046e2e9ccf9cf972cd95b6219714d3d2
Alexandre Vaz dos Santos <alexxvaz@yahoo.com.br>
2022-11-09 12:18:21 -0300

    Mensagem – Tratar o erro exibindo mensagens na página

--- TSALX/Content/script/js/tsalx.js
+++ TSALX/Content/script/js/tsalx.js
@@ -15,3 +15,37 @@
     $('#RegiaoTabela').DataTable(arrOpcao);
 
 });
+
+function exibirMensagem(tipo, mensagem) {
+
+    if (mensagem != "") {
+
+        toastr.options = {
+            "closeButton": true,
+            "debug": false,
+            "newestOnTop": false,
+            "progressBar": false,
+            "positionClass": "toast-top-full-width",
+            "preventDuplicates": true,
+            "onclick": null,
+            "showDuration": "500",
+            "hideDuration": "1000",
+            "timeOut": "5000",
+            "extendedTimeOut": "1000",
+            "showEasing": "swing",
+            "hideEasing": "linear",
+            "showMethod": "fadeIn",
+            "hideMethod": "fadeOut"
+        };
+
+        var sTipo = "error";
+
+        if (tipo == 0) { 
+            sTipo = "success";
+        } else if (tipo == 1) {   // Processo
+            sTipo = "warning";
+        }
+
+        toastr[sTipo](mensagem, "Trade Sport ALX");
+    }
+}
---
+++ TSALX/ViewModel/basePagina.cs
@@ -0,0 +1,9 @@
+﻿namespace TSALX.ViewModel
+{
+    public abstract class basePagina
+    {
+        public string Titulo { get; set; }
+        public short TipoMSG { get; set; }
+        public string TextoMSG { get; set; }
+    }
+}
